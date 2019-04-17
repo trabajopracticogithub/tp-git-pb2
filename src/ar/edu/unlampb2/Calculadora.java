3c2c110ed216bfb3b1dfd4207bd39099d10bd46b
TestJuanGuerrero <xthemagicjuanx@gmail.com>
2019-04-17 01:05:26 -0300

    Cambie la estructura de las operaciones, mejoré algunos cambios y puse default en el ciclo switch

--- src/ar/edu/unlampb2/Calculadora.java
+++ src/ar/edu/unlampb2/Calculadora.java
@@ -12,34 +12,54 @@ public class Calculadora {
 
 		Scanner teclado = new Scanner(System.in);
 
-		do {
-			System.out.println(
-					"Ingrese su operación \n 1 para multiplicar \n 2 para restar \n 3 para sumar \n 4 para dividir");
-			opcion = teclado.nextInt();
+		
+		System.out.println("Ingrese su operación \n 1 para suma \n 2 para restar \n 3 para multiplicar \n 4 para dividir");
+		opcion = teclado.nextInt();
 
-		} while (opcion < 1 && opcion > 4);
 
-		System.out.println("Ingrese el primer numero");
-		a = teclado.nextDouble();
-
-		System.out.println("Ingrese el segundo numero");
-		b = teclado.nextDouble();
-
-		switch(opcion){
-			
+		
+			switch(opcion){
 			case 1:
-			  System.out.println("El resultado de la multiplicacion es : "+(resultado=a*b));
+			   
+			  System.out.println("Ingrese el primer numero");
+		      a = teclado.nextDouble();
+
+		      System.out.println("Ingrese el segundo numero");
+		      b = teclado.nextDouble();
+
+			  System.out.println("El resultado de la suma es : "+(resultado=a+b));
 			break;
 
 			case 2:
+
+			  System.out.println("Ingrese el primer numero");
+		      a = teclado.nextDouble();
+
+		      System.out.println("Ingrese el segundo numero");
+		      b = teclado.nextDouble();
+
 			  System.out.println("El resultado de la resta es : "+(resultado=a-b));
 			break;
 
 			case 3:
-			  System.out.println("El resultado de la suma es : "+(resultado=a+b));
+			  
+			  System.out.println("Ingrese el primer numero");
+		      a = teclado.nextDouble();
+
+		      System.out.println("Ingrese el segundo numero");
+		      b = teclado.nextDouble();
+
+			  System.out.println("El resultado de la multiplicacion es : "+(resultado=a*b));
 			break;
 
 			case 4:
+			 System.out.println("Ingrese el primer numero");
+		      a = teclado.nextDouble();
+
+		      System.out.println("Ingrese el segundo numero");
+		      b = teclado.nextDouble();
+
+
 			if(b!=0){
 			  System.out.println("El resultado de la division es : "+(resultado=a/b));
 			}
@@ -47,6 +67,12 @@ public class Calculadora {
 				System.out.println("La division por 0 no existe");
 			}
 			break;
+
+			default:
+			  System.out.println("Ingresaste la operacion incorrecta");
+
+			break;
+	
 		}
 
 	}
